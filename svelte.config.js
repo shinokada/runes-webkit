d0cc292cb39fafb4c24c0ba2b7d0045b4943ecbd
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-04-01 18:28:57 +0200

    fix: add runes to svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,9 @@ const config = {
       postcss: true
     })
   ],
-
+  compilerOptions: {
+    runes: true
+  },
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.

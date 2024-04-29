413fadc0fc99f27339ebf65857659d7a9ac15015
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-04-29 17:50:44 +0200

    feat: update props pages

--- src/routes/guide/svelte-4/props/+page.ts
+++ src/routes/guide/svelte-4/props/+page.ts
@@ -2,15 +2,15 @@ import type { MetaProps } from 'runes-meta-tags';
 
 export const load = () => {
   const pageMetaTags = Object.freeze({
-    title: 'Svelte 4 Props example - Runes Webkit',
-    description: 'Svelte 4 Props example',
+    title: 'Svelte 4 Props - Svelte Awesome Icons',
+    description: 'Svelte Awesome Icons Props for Svelte 4',
     og: {
-      title: 'Svelte 4 Props example - Runes Webkit',
-      description: 'Svelte 4 Props example',
+      title: 'Svelte 4 Props - Svelte Awesome Icons',
+      description: 'Svelte Awesome Icons Props for Svelte 4',
     },
     twitter: {
-      title: 'Svelte 4 Props example - Runes Webkit',
-      description: 'Svelte 4 Props example',
+      title: 'Svelte 4 Props - Svelte Awesome Icons',
+      description: 'Svelte Awesome Icons Props for Svelte 4',
     }
   }) satisfies MetaProps;
   return { pageMetaTags };
--- src/routes/guide/svelte-5/props/+page.ts
+++ src/routes/guide/svelte-5/props/+page.ts
@@ -2,14 +2,14 @@ import type { MetaProps } from 'runes-meta-tags';
 
 export const load = () => {
   const pageMetaTags = Object.freeze({
-    title: 'Svelte 5 Props example - Runes Webkit',
+    title: 'Svelte 5 Props example - Svelte Ant Design Icons',
     description: 'Svelte 5 Props example',
     og: {
-      title: 'Svelte 5 Props example - Runes Webkit',
+      title: 'Svelte 5 Props example - Svelte Ant Design Icons',
       description: 'Svelte 5 Props example',
     },
     twitter: {
-      title: 'Svelte 5 Props example - Runes Webkit',
+      title: 'Svelte 5 Props example - Svelte Ant Design Icons',
       description: 'Svelte 5 Props example',
     }
   }) satisfies MetaProps;

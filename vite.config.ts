ef82703ec4d6eeba043e3785b0419b3f95ccc10c
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-05-31 08:01:49 +0200

    chore: cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -15,23 +15,14 @@ export default defineConfig({
     __NAME__: JSON.stringify(pkg.name),
     __DESCRIPTION__: JSON.stringify(pkg.description),
     __VERSION__: JSON.stringify(pkg.version),
-    // `"${pkg.repository.url}"`
     __GITHUBURL__: JSON.stringify(pkg.repository.url),
-    // `"${pkg.contributors[0].name}"`
     __ORIGINAL_SOURCE_NAME__: JSON.stringify(pkg.contributors[0].name),
-    // `"${pkg.contributors[0].url}"`
     __ORIGINAL_SOURCE_URL__: JSON.stringify(pkg.contributors[0].url),
-    // `"${runaticsPackage.version}"`
     __RUNATICS_VERSION__: JSON.stringify(runaticsPackage.version),
-    // `"${runesmetatagsPackage.version}"`
     __RUNES_METATAGS_VERSION__: JSON.stringify(runesmetatagsPackage.version),
-    // `"${sveltePackage.version}"`
     __SVELTE_VERSION__: JSON.stringify(sveltePackage.version),
-    // `"${svelteKitPackage.version}"`
     __SVELTEKIT_VERSION__: JSON.stringify(svelteKitPackage.version),
-// `"${svelterunehighlightPackage.version}"`
     __SVELTE_RUNE_HIGHLIGHT_VERSION__: JSON.stringify(svelterunehighlightPackage.version),
-    // `"${svelte5uilibPackage.version}"`
     __SVELTE_5_UI_LIB_VERSION__: JSON.stringify(svelte5uilibPackage.version),
     __VITE_VERSION__: JSON.stringify(vitePackage.version),
   }

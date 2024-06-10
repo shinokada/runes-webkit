104bcd56d428e9a417b5dc95903f4969488ff6a9
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-06-10 21:07:09 +0200

    Update Svelte to next

--- tests/icons.test.ts
+++ tests/icons.test.ts
@@ -9,7 +9,7 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('Icons page has expected h1, meta title', async ({ page }) => {
-  await expect(page.getByRole('heading', { name: title })).toBeVisible();
+  await expect(page.getByRole('heading', { name: title, level: 1 })).toBeVisible();
 });
 
 test('Icons page has expected meta title', async ({ page }) => {

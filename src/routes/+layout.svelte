0c564f78a2d8d76cf5a1a98480ef631622be0644
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-09-21 06:51:25 +0200

    fix: adding if statement for showing on this page to certain pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -211,9 +211,11 @@
       </SidebarGroup>
     </Sidebar>
   {/if}
+  {#if urlsToIncludeSwitcherAndSidebar.some((path) => currentUrl.startsWith(path))}
   <div class="relative">
     <OnThisPage {extract} headingSelector="#mainContent > :where(h2, h3)" />
   </div>
+  {/if}
     {@render children()}
 </div>
 <Footer {brand} {lis} />
